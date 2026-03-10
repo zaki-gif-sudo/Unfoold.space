3fde133889171d0b64b844932b13de3537cb1798
Zaki <alisaparizaki@gmail.com>
2026-03-11 00:04:11 +0700

    fix: remove complex timeout handler from pocketbaseClient - was causing request hangs

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -4,26 +4,6 @@ const POCKETBASE_API_URL = import.meta.env.VITE_POCKETBASE_URL || 'https://api.u
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
 
-// Add request timeout using AbortController
-pocketbaseClient.beforeSend = function(url, init) {
-  const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), 10000); // 10 second timeout
-  
-  const originalFetch = init?.fetch || fetch;
-  init.fetch = async (req) => {
-    try {
-      const response = await originalFetch(req);
-      clearTimeout(timeoutId);
-      return response;
-    } catch (error) {
-      clearTimeout(timeoutId);
-      throw error;
-    }
-  };
-  
-  return { url, init };
-};
-
 export default pocketbaseClient;
 
 export { pocketbaseClient };
